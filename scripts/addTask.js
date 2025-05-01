3cbde81d0843d016e8cd5d712db52594c5eef2fc
gustavo <guillones11@gmail.com>
2025-05-01 11:29:54 -0300

    Adicionando *enum* no html e try catch na requisacao

--- scripts/addTask.js
+++ scripts/addTask.js
@@ -9,15 +9,19 @@ function salvarTask(){
         descricao: descricao.value,
         tipo: tipo.value
     };
-
-    fetch("http://localhost:3000/tasks", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(task)
-    })
-    clearField();
+    try{
+        fetch("http://localhost:3000/tasks", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(task)
+        })
+        clearField();
+        console.log('Tarefa salva com sucesso');
+    }catch(error){
+        console.error('Falha ao salvar tarefa',error);
+    }
 }
 
 function clearField(){
